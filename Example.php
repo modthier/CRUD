c28c5495d9c075f7a439120ebe2e799b52a6b981
modthier <mod47387@gmail.com>
2022-03-29 09:42:45 +0200

    Update Example.php

--- Example.php
+++ Example.php
@@ -1,27 +1,21 @@
 
 <?php session_start();
     // this is an example of using curd trait    
-    include '../../../inc/conn.php';
-    include '../../../classes/init.php';
+    include 'conn.php'; // include your connection to database
+    
+    $root = new root($conn);
 
-    $inc->checkEmpty($_POST);
-    $inc->checkName($_POST['insurance_type']);
+    $root->checkEmpty($_POST);
+    
 
-    $error = $inc->getErrors();
+    $error = $root->getErrors();
 
     if(empty($error)){
-        $success = $inc->save('insurance_types',$_POST);
-        if($success) {
-            $_SESSION['done'] = "Data Saved";
-            header("Location:../../index.php?view=inc");
-            exit();
-        }else {
-            $_SESSION['error'] = "Error Happend try a gain";
-            header("Location:../../index.php?view=inc");
-            exit();
-        }
+        $success = $inc->saveWithoutId('tableName_here',$_POST);
+        header("Location:index.php?view=inc");
+        exit();
     }else {
         $_SESSION['errArr'] = $error;
-        header("Location:../../index.php?view=inc");
+        header("Location:index.php?view=inc");
         exit();
     }
